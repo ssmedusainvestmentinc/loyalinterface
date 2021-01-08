f99db19f917e4d0be3f195286dda23058e274ea3
ssmedusainvestmentinc <65372592+ssmedusainvestmentinc@users.noreply.github.com>
2021-01-07 20:14:35 -0800

    Update settings.py

--- mezzanine/project_template/project_name/settings.py
+++ mezzanine/project_template/project_name/settings.py
@@ -3,7 +3,17 @@ import os
 from django import VERSION as DJANGO_VERSION
 from django.utils.translation import ugettext_lazy as _
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 ######################
 # MEZZANINE SETTINGS #
 ######################
